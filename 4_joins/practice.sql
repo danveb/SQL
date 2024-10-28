2145728f8cb1312b56612ea79c9de8621ac28393
Daniel Bae <danibae91@icloud.com>
2024-10-28 20:33:02 -0300

    push again

--- 4_joins/practice.sql
+++ 4_joins/practice.sql
@@ -21,3 +21,31 @@ WHERE status = 'Completed'
 GROUP BY users.city 
 ORDER BY MAX(trades.quantity) DESC
 LIMIT 3; 
+
+-- Exercise 2: list all books and their authors 
+-- Show name of each author together with title of the book they wrote and the year which that book was published 
+
+-- FROM books TABLE 
+-- INNER JOIN author 
+-- ON books.author_id = author.id
+-- SELECT author.name, book.title, book.publish_year 
+
+SELECT author.name, book.title, book.publish_year 
+FROM books 
+INNER JOIN author 
+ON books.author_id = author.id 
+
+-- Exercise 3: list authors and books published after 2005 
+-- show the name of each author together with title of the book they wrote and the year in which the book was published. Show only books published after 2005 
+
+-- FROM author TABLE  
+-- INNER JOIN books TABLE
+-- ON authors.id = books.author_id
+-- SELECT author.name, book.title, book.publish_year 
+-- WHERE publish_year = 2005; 
+
+SELECT author.name, book.title, book.publish_year 
+FROM author 
+INNER JOIN book 
+ON author.id = book.author_id 
+WHERE publish_year = 2005; 
